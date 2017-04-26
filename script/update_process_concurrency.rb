ff645a6677f040f8ffaa480c9518cb62ae03ed48
Adam Cooke <me@adamcooke.io>
2017-04-26 20:48:04 +0100

    add script to set process concurrency on start/restart/run from config

---
+++ script/update_process_concurrency.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require File.expand_path('../../lib/postal/config', __FILE__)
+worker_quantity = Postal.config.workers&.quantity || 1
+hash = {
+  'processes' => {
+    'worker' => {
+      'quantity' => worker_quantity
+    },
+    'fast' => {
+      'quantity' => Postal.config.fast_server ? 1 : 0
+    }
+  }
+}.to_yaml
+
+File.open(Postal.app_root.join('Procfile.local'), 'w') { |f| f.write(hash + "\n")}
