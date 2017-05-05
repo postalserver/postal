5d0b8eb16de2495059f25c4cae45b75c40a34f9e
Adam Cooke <me@adamcooke.io>
2017-05-05 11:15:10 +0100

    add some docs to ubuntu install script

--- script/install/ubuntu1604.sh
+++ script/install/ubuntu1604.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# This will install everything required to run a basic Postal installation.
+# This should be run on a clean Ubuntu 16.04 server.
+#
+# Once the installation has completed you will be able to access the Postal web
+# interface on port 443. It will have a self-signed certificate.
+#
+# * Change the MySQL & RabbitMQ passwords
+# * Create your first admin user with 'postal make-user'
+# * Replace the self-signed certificate in /etc/nginx/ssl/postal.cert
+# * Make appropriate changes to the configuration in /opt/postal/config/postal.yml
+# * Setup your DNS                          [ https://github.com/atech/postal/wiki/Domains-&-DNS-Configuration ]
+# * Configure the click & open tracking     [ https://github.com/atech/postal/wiki/Click-&-Open-Tracking ]
+# * Configure spam & virus checking         [ https://github.com/atech/postal/wiki/Spam-&-Virus-Checking ]
+
 set -e
 
 apt install -y software-properties-common
@@ -39,3 +53,6 @@ cp /opt/postal/app/resource/nginx.cfg /etc/nginx/sites-available/default
 mkdir /etc/nginx/ssl/
 openssl req -x509 -newkey rsa:4096 -keyout /etc/nginx/ssl/postal.key -out /etc/nginx/ssl/postal.crt -days 365 -nodes -subj "/C=GB/ST=Example/L=Example/O=Example/CN=example.com"
 service nginx reload
+
+echo
+echo "Installation complete"
