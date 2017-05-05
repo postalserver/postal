1758dc466a333f5ba39c2c67fe6a4306fd140a54
Adam Cooke <me@adamcooke.io>
2017-05-05 19:55:20 +0100

    reset before pulling to avoid issues

--- script/generate_api_docs.sh
+++ script/generate_api_docs.sh
@@ -6,8 +6,8 @@ if [ ! -d /tmp/postal-api/.git ];
 then
   git clone git@github.com:atech/postal-api /tmp/postal-api
 else
-  git -C /tmp/postal-api pull origin master
   git -C /tmp/postal-api reset --hard HEAD
+  git -C /tmp/postal-api pull origin master
 fi
 
 rm -Rf /tmp/postal-api/*
