553aedf0317641761517d711066fa21488297a00
Adam Cooke <me@adamcooke.io>
2017-05-05 19:54:52 +0100

    name the authenticator

--- api/authenticator.rb
+++ api/authenticator.rb
@@ -1,4 +1,5 @@
 authenticator :server do
+  friendly_name "Server Authenticator"
   header "X-Server-API-Key", "The API token for a server that you wish to authenticate with.", :example => 'f29a45f0d4e1744ebaee'
   error 'InvalidServerAPIKey', "The API token provided in X-Server-API-Key was not valid.", :attributes => {:token => "The token that was looked up"}
   error 'ServerSuspended', "The mail server has been suspended"
