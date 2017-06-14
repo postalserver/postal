126a59200a3140a638a7b0554075af09031ac536
Adam Cooke <me@adamcooke.io>
2017-06-14 09:30:01 +0100

    add script to return current queue size (for monitoring)

---
+++ script/queue_size.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+require_relative '../lib/postal/config'
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => Postal.config.main_db.host, :username => Postal.config.main_db.username, :password => Postal.config.main_db.password, :port => Postal.config.main_db.port, :database => Postal.config.main_db.database)
+result = client.query("SELECT COUNT(id) as size FROM `queued_messages` WHERE retry_after IS NULL OR retry_after > ADDTIME(UTC_TIMESTAMP(), '30')")
+puts result.to_a.first['size']
