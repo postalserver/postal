3d8d3956fec820baa688af64d0c2d548b3c3bca3
Petter Helset <petter@helset.eu>
2017-10-26 13:49:57 +0200

    Change SQL charset for message_db to utf8mb4

--- lib/postal/message_db/mysql.rb
+++ lib/postal/message_db/mysql.rb
@@ -7,7 +7,7 @@ module Postal
       # it would be undesirable as we'd just end up with lots of connections.
 
       def self.new_client
-        Mysql2::Client.new(:host => Postal.config.message_db.host, :username => Postal.config.message_db.username, :password => Postal.config.message_db.password, :port => Postal.config.message_db.port, :reconnect => true)
+        Mysql2::Client.new(:host => Postal.config.message_db.host, :username => Postal.config.message_db.username, :password => Postal.config.message_db.password, :port => Postal.config.message_db.port, :reconnect => true, :encoding => Postal.config.message_db.encoding || 'utf8mb4')
       end
 
       @free_clients = []
