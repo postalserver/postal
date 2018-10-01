5309af29f5adbb912af842e6bbcf014ff5b351c4
Lukas M <lukas.mrtvy@gmail.com>
2018-10-01 09:43:36 +0200

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -3,10 +3,15 @@ threads_count = Postal.config.web_server&.max_threads&.to_i || 5
 threads         threads_count, threads_count
 bind_address  = Postal.config.web_server&.bind_address || '127.0.0.1'
 bind_port     = Postal.config.web_server&.port&.to_i || 5000
-bind            "tcp://#{bind_address}:#{bind_port}"
 environment     Postal.config.rails&.environment || 'development'
 prune_bundler
 quiet false
+
+if ssl_enabled
+	bind  "ssl://#{bind_address}:#{bind_port}?key=#{server_key}&cert=#{server_crt}&ssl_cipher_list=#{ssl_ciphers}"
+else
+	bind  "tcp://#{bind_address}:#{bind_port}"
+
 unless ENV['LOG_TO_STDOUT']
   stdout_redirect Postal.log_root.join('puma.log'), Postal.log_root.join('puma.log'), true
 end
