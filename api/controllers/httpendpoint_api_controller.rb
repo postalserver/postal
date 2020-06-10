31e561638ac47c64a16f175d79af7a63c1443f61
reza <kinggarethbale@gmail.com>
2020-06-10 15:29:27 +0430

    api does not work with id.

--- api/controllers/httpendpoint_api_controller.rb
+++ api/controllers/httpendpoint_api_controller.rb
@@ -1,141 +1,143 @@
 controller :httpendpoints do
-    friendly_name "HttpEndpoint API"
-    description "This API allows you to create and view http endpoints on server"
-    authenticator :server
-          
-    action :create do
-      title "Create http endpoint"
-      description "This action allows you to create http endpoints"
+  friendly_name "HttpEndpoint API"
+  description "This API allows you to create and view http endpoints on server"
+  authenticator :server
+        
+  action :create do
+    title "Create http endpoint"
+    description "This action allows you to create http endpoints"
 
-      param :name, "Endpoint name", :required => true, :type => String
-      param :url, "Endpoint url", :required => true, :type => String
-      param :encoding, "Endpoint encoding: BodyAsJSON | FormData", :required => true, :type => String, :default => "BodyAsJSON"
-      param :format, "Endpoint format: Hash | RawMessage", :required => true, :type => String, :default => "RawMessage"
-      param :strip_replies, "Endpoint strip replies", :required => true, :type => String, :default => "yes"
-      param :include_attachments, "Endpoint include attachments", :required => true, :type => String, :default => "yes"
-      param :timeout, "Endpoint timeout default: 5s", :required => false, :type => Integer, :default => 5
+    param :name, "Endpoint name", :required => true, :type => String
+    param :url, "Endpoint url", :required => true, :type => String
+    param :encoding, "Endpoint encoding: BodyAsJSON | FormData", :required => true, :type => String, :default => "BodyAsJSON"
+    param :format, "Endpoint format: Hash | RawMessage", :required => true, :type => String, :default => "RawMessage"
+    param :strip_replies, "Endpoint strip replies", :required => true, :type => String, :default => "yes"
+    param :include_attachments, "Endpoint include attachments", :required => true, :type => String, :default => "yes"
+    param :timeout, "Endpoint timeout default: 5s", :required => false, :type => Integer, :default => 5
 
-      error 'ValidationError', "The provided data was not sufficient to create http endpoint", :attributes => {:errors => "A hash of error details"}
-      error 'HttpEndPointNameMissing', "HttpEndPoint name is missing"
-      error 'InvalidHttpEndPointName', "HttpEndPoint name is invalid"
-      error 'HttpEndPointNameExists', "HttpEndPoint name already exists"
+    error 'ValidationError', "The provided data was not sufficient to create http endpoint", :attributes => {:errors => "A hash of error details"}
+    error 'HttpEndPointNameMissing', "HttpEndPoint name is missing"
+    error 'InvalidHttpEndPointName', "HttpEndPoint name is invalid"
+    error 'HttpEndPointNameExists', "HttpEndPoint name already exists"
 
-      returns Hash, :structure => :httpendpoint
+    returns Hash, :structure => :httpendpoint
 
-      action do
-        httpendpoint = identity.server.http_endpoints.find_by_name(params.name)
-        if httpendpoint.nil?
-          httpendpoint = HTTPEndpoint.new
-          httpendpoint.server = identity.server
-          httpendpoint.name = params.name
-          httpendpoint.url = params.url
-          httpendpoint.encoding = params.encoding
-          httpendpoint.format = params.format
-          httpendpoint.strip_replies = params.strip_replies == 'yes' ? true : false
-          httpendpoint.include_attachments = params.include_attachments =='yes' ? true : false
-          httpendpoint.timeout = params.timeout
-          httpendpoint.created_at = Time.now
-          httpendpoint.updated_at = Time.now
-          if httpendpoint.save
-            structure :httpendpoint, httpendpoint, :return => true
-          else
-            error_message = httpendpoint.errors.full_messages.first
-            if error_message == "Name is invalid"
-              error "InvalidHttpEndPointName"
-            else
-              error "Unknown Error", error_message
-            end
-          end
-        else
-          error 'HttpEndPointNameExists'
-        end
-      end
-    end
-
-    action :query do
-      title "Query http endpoint"
-      description "This action allows you to query http endpoint"
-
-      param :id, "Id of http endpoint", :required => true, :type => Integer
-
-      error 'ValidationError', "The provided data was not sufficient to query http endpoint", :attributes => {:errors => "A hash of error details"}
-      error 'HttpEndpointIdMissing', "HttpEndpoint id is missing"
-      error 'HttpEndpointNotFound', "The http endpoint not found"
-
-      returns Hash, :structure => :httpendpoint
-
-      action do
-        httpendpoint = identity.server.http_endpoints.find_by_id(params.id)
-        if httpendpoint.nil?
-          error 'HttpEndpointNotFound'
-        else
+    action do
+      httpendpoint = identity.server.http_endpoints.find_by_name(params.name)
+      if httpendpoint.nil?
+        httpendpoint = HTTPEndpoint.new
+        httpendpoint.server = identity.server
+        httpendpoint.name = params.name
+        httpendpoint.url = params.url
+        httpendpoint.encoding = params.encoding
+        httpendpoint.format = params.format
+        httpendpoint.strip_replies = params.strip_replies == 'yes' ? true : false
+        httpendpoint.include_attachments = params.include_attachments =='yes' ? true : false
+        httpendpoint.timeout = params.timeout
+        httpendpoint.created_at = Time.now
+        httpendpoint.updated_at = Time.now
+        if httpendpoint.save
           structure :httpendpoint, httpendpoint, :return => true
-        end
-      end
-    end
-
-    action :update do
-      title "Update http endpoint"
-      description "This action allows you to update http endpoint"
-
-      param :name, "Endpoint name", :required => false, :type => String
-      param :url, "Endpoint url", :required => false, :type => String
-      param :encoding, "Endpoint encoding: BodyAsJSON | FormData", :required => false, :type => String
-      param :format, "Endpoint format: Hash | RawMessage", :required => false, :type => String
-      param :strip_replies, "Endpoint strip replies", :required => false, :type => String
-      param :include_attachments, "Endpoint include attachments", :required => false, :type => String
-      param :timeout, "Endpoint timeout default: 5s", :required => false, :type => Integer, :default => 5
-      param :id, "Id of http endpoint", :required => true, :type => Integer
-
-      error 'ValidationError', "The provided data was not sufficient to query http endpoint", :attributes => {:errors => "A hash of error details"}
-      error 'HttpEndpointIdMissing', "HttpEndpoint id is missing"
-      error 'HttpEndpointNotFound', "The http endpoint not found"
-
-      action do
-        httpendpoint = identity.server.http_endpoints.find_by_id(params.id)
-        if httpendpoint.nil?
-          error "HttpEndpointNotFound"
         else
-          httpendpoint.name = params.name if !params.name.nil?
-          httpendpoint.url = params.url if !params.url.nil? 
-          httpendpoint.encoding = params.encoding if !params.encoding.nil?
-          httpendpoint.format = params.format if !params.format.nil?
-          httpendpoint.strip_replies = (params.strip_replies == 'yes' ? true : false) if !params.strip_replies.nil?
-          httpendpoint.include_attachments = (params.include_attachments =='yes' ? true : false) if !params.include_attachments.nil?
-          httpendpoint.timeout = params.timeout if !params.timeout.nil?
-          httpendpoint.updated_at = Time.now
-          if httpendpoint.save
-            structure :httpendpoint, httpendpoint, :return => true
+          error_message = httpendpoint.errors.full_messages.first
+          if error_message == "Name is invalid"
+            error "InvalidHttpEndPointName"
           else
-            error "Unknown Error", httpendpoint.errors.full_messages.first
+            error "Unknown Error", error_message
           end
         end
+      else
+        error 'HttpEndPointNameExists'
       end
     end
+  end
 
-    action :delete do
-      title "Delete a http endpoint"
-      description "This action allows you to delete http endpoint"
+  action :query do
+    title "Query http endpoint"
+    description "This action allows you to query http endpoint"
 
-      param :id, "Id of http endpoint", :required => true, :type => Integer
+    param :name, "Endpoint name", :required => true, :type => String
+    param :url, "Endpoint url", :required => true, :type => String
 
-      error 'ValidationError', "The provided data was not sufficient to query http endpoint", :attributes => {:errors => "A hash of error details"}
-      error 'HttpEndpointIdMissing', "HttpEndpoint id is missing"
-      error 'HttpEndpointNotFound', "The http endpoint not found"
-      error 'HttpEndpointNotDeleted', "HttpEndpoint could not be deleted"
+    error 'ValidationError', "The provided data was not sufficient to query http endpoint", :attributes => {:errors => "A hash of error details"}
+    error 'HttpEndpointIdMissing', "HttpEndpoint id is missing"
+    error 'HttpEndpointNotFound', "The http endpoint not found"
 
+    returns Hash, :structure => :httpendpoint
 
-      returns Hash, :structure => :httpendpoint
-      
-      action do
-        httpendpoint = identity.server.http_endpoints.find_by_id(params.id)
-        if httpendpoint.nil?
-          error 'HttpEndpointNotFound'
-        elsif httpendpoint.delete
-          {:message => "HttpEndpoint deleted successfully"}
+    action do
+      httpendpoint = identity.server.http_endpoints.find_by_name_and_url(params.name, params.url)
+      if httpendpoint.nil?
+        error 'HttpEndpointNotFound'
+      else
+        structure :httpendpoint, httpendpoint, :return => true
+      end
+    end
+  end
+
+  action :update do
+    title "Update http endpoint"
+    description "This action allows you to update http endpoint"
+
+    param :name, "Endpoint name", :required => false, :type => String
+    param :url, "Endpoint url", :required => false, :type => String
+    param :encoding, "Endpoint encoding: BodyAsJSON | FormData", :required => false, :type => String
+    param :format, "Endpoint format: Hash | RawMessage", :required => false, :type => String
+    param :strip_replies, "Endpoint strip replies", :required => false, :type => String
+    param :include_attachments, "Endpoint include attachments", :required => false, :type => String
+    param :timeout, "Endpoint timeout default: 5s", :required => false, :type => Integer, :default => 5
+    param :id, "Id of http endpoint", :required => true, :type => Integer
+
+    error 'ValidationError', "The provided data was not sufficient to query http endpoint", :attributes => {:errors => "A hash of error details"}
+    error 'HttpEndpointIdMissing', "HttpEndpoint id is missing"
+    error 'HttpEndpointNotFound', "The http endpoint not found"
+
+    action do
+      httpendpoint = identity.server.http_endpoints.find_by_id(params.id)
+      if httpendpoint.nil?
+        error "HttpEndpointNotFound"
+      else
+        httpendpoint.name = params.name if !params.name.nil?
+        httpendpoint.url = params.url if !params.url.nil? 
+        httpendpoint.encoding = params.encoding if !params.encoding.nil?
+        httpendpoint.format = params.format if !params.format.nil?
+        httpendpoint.strip_replies = (params.strip_replies == 'yes' ? true : false) if !params.strip_replies.nil?
+        httpendpoint.include_attachments = (params.include_attachments =='yes' ? true : false) if !params.include_attachments.nil?
+        httpendpoint.timeout = params.timeout if !params.timeout.nil?
+        httpendpoint.updated_at = Time.now
+        if httpendpoint.save
+          structure :httpendpoint, httpendpoint, :return => true
         else
-          error 'HttpEndpointNotDeleted'
+          error "Unknown Error", httpendpoint.errors.full_messages.first
         end
       end
     end
   end
+
+  action :delete do
+    title "Delete a http endpoint"
+    description "This action allows you to delete http endpoint"
+
+    param :name, "Endpoint name", :required => true, :type => String
+    param :url, "Endpoint url", :required => true, :type => String
+
+    error 'ValidationError', "The provided data was not sufficient to query http endpoint", :attributes => {:errors => "A hash of error details"}
+    error 'HttpEndpointIdMissing', "HttpEndpoint id is missing"
+    error 'HttpEndpointNotFound', "The http endpoint not found"
+    error 'HttpEndpointNotDeleted', "HttpEndpoint could not be deleted"
+
+
+    returns Hash, :structure => :httpendpoint
+    
+    action do
+      httpendpoint = identity.server.http_endpoints.find_by_name_and_url(params.name, params.url)
+      if httpendpoint.nil?
+        error 'HttpEndpointNotFound'
+      elsif httpendpoint.delete
+        {:message => "HttpEndpoint deleted successfully"}
+      else
+        error 'HttpEndpointNotDeleted'
+      end
+    end
+  end
+end
--- api/controllers/routes_api_controller.rb
+++ api/controllers/routes_api_controller.rb
@@ -1,166 +1,159 @@
 controller :routes do
-    friendly_name "Routes API"
-    description "This API allows you to create and view routes on server"
-    authenticator :server
-          
-    action :create do
-      title "Create a route"
-      description "This action allows you to create routes"
+  friendly_name "Routes API"
+  description "This API allows you to create and view routes on server"
+  authenticator :server
+        
+  action :create do
+    title "Create a route"
+    description "This action allows you to create routes"
 
-      param :domain, "Domain name", :required => true, :type => String
-      param :endpoint_name, "Endpoint address (ex@mp.le)", :required => true, :type => String
-      param :endpoint_type, "Endpoint type: SMTPEndpoint | HTTPEndpoint | AddressEndpoint", :required => true, :type => String, :default => "HTTPEndpoint"
-      param :spam_mode, "Route spam mode: Mark | Quarantine | Fail", :required => true, :type => String, :default => "Mark"
-      param :mode, "Route mode: Endpoint | Accept | Hold | Bounce | Reject", :required => true, :type => String, :default => "Endpoint"
-      param :name, "Route name (max 50)", :required => true, :type => String
-  
-      error 'ValidationError', "The provided data was not sufficient to create route", :attributes => {:errors => "A hash of error details"}
-      error 'InvalidEndPoint', "The endpoint: {type} with name/address: {name} dose not exists", :attributes => {:type => "Endpoint type", :name => "Endpoint name"}
-      error 'RouteNameMissing', "Route name is missing"
-      error 'InvalidRouteName', "Route name is invalid"
-      error 'RouteNameExists', "Route name already exists"
+    param :domain, "Domain name", :required => true, :type => String
+    param :endpoint_name, "Endpoint address (ex@mp.le)", :required => true, :type => String
+    param :endpoint_type, "Endpoint type: SMTPEndpoint | HTTPEndpoint | AddressEndpoint", :required => true, :type => String, :default => "HTTPEndpoint"
+    param :spam_mode, "Route spam mode: Mark | Quarantine | Fail", :required => true, :type => String, :default => "Mark"
+    param :mode, "Route mode: Endpoint | Accept | Hold | Bounce | Reject", :required => true, :type => String, :default => "Endpoint"
+    param :name, "Route name (max 50)", :required => true, :type => String
 
-      returns Hash, :structure => :route
-  
-      action do
-        route = identity.server.routes.find_by_name_and_domain(params.name, params.domain)
-        if route.nil?
-          route = Route.new
-          route.domain = identity.server.domains.find_by_name(params.domain)
-          route.server = identity.server
-          route.name = params.name
-          route.spam_mode = params.spam_mode
-          route.endpoint_type = params.endpoint_type
-          if params.endpoint_type == 'HTTPEndpoint'
-            if identity.server.http_endpoints.find_by_name(params.endpoint_name).nil?
-              error "InvalidEndPoint", :type => params.endpoint_type, :name => params.endpoint_name
-            end
-            route.endpoint_id = identity.server.http_endpoints.find_by_name(params.endpoint_name).id
-          elsif params.endpoint_type == 'SMTPEndpoint'
-            if identity.server.smtp_endpoints.find_by_name(params.endpoint_name).nil?
-              error "InvalidEndPoint", :type => params.endpoint_type, :name => params.endpoint_name
-            end
-            route.endpoint_id = identity.server.smtp_endpoints.find_by_name(params.endpoint_name).id
-          else
-            if identity.server.address_endpoints.find_by_address(params.endpoint_name).nil?
-              error "InvalidEndPoint", :type => params.endpoint_type, :name => params.endpoint_name
-            end
-            route.endpoint_id = identity.server.address_endpoints.find_by_address(params.endpoint_name).id
+    error 'ValidationError', "The provided data was not sufficient to create route", :attributes => {:errors => "A hash of error details"}
+    error 'InvalidEndPoint', "The endpoint: {type} with name/address: {name} dose not exists", :attributes => {:type => "Endpoint type", :name => "Endpoint name"}
+    error 'RouteNameMissing', "Route name is missing"
+    error 'InvalidRouteName', "Route name is invalid"
+    error 'RouteNameExists', "Route name already exists"
+
+    returns Hash, :structure => :route
+
+    action do
+      route = identity.server.routes.find_by_name_and_domain(params.name, params.domain)
+      if route.nil?
+        route = Route.new
+        route.domain = identity.server.domains.find_by_name(params.domain)
+        route.server = identity.server
+        route.name = params.name
+        route.spam_mode = params.spam_mode
+        route.endpoint_type = params.endpoint_type
+        if params.endpoint_type == 'HTTPEndpoint'
+          if identity.server.http_endpoints.find_by_name(params.endpoint_name).nil?
+            error "InvalidEndPoint", :type => params.endpoint_type, :name => params.endpoint_name
           end
-          route.mode = params.mode
-          route.created_at = Time.now
-          route.updated_at = Time.now
-          if route.save
-            structure :route, route, :return => true
-          else
-            error_message = route.errors.full_messages.first
-            if error_message == "Name is invalid"
-              error "InvalidRouteName"
-            else
-              error "Unknown Error", error_message
-            end
+          route.endpoint_id = identity.server.http_endpoints.find_by_name(params.endpoint_name).id
+        elsif params.endpoint_type == 'SMTPEndpoint'
+          if identity.server.smtp_endpoints.find_by_name(params.endpoint_name).nil?
+            error "InvalidEndPoint", :type => params.endpoint_type, :name => params.endpoint_name
           end
+          route.endpoint_id = identity.server.smtp_endpoints.find_by_name(params.endpoint_name).id
         else
-          error 'RouteNameExists'
+          if identity.server.address_endpoints.find_by_address(params.endpoint_name).nil?
+            error "InvalidEndPoint", :type => params.endpoint_type, :name => params.endpoint_name
+          end
+          route.endpoint_id = identity.server.address_endpoints.find_by_address(params.endpoint_name).id
         end
-  
-      end
-    end
-    
-    action :query do
-      title "Query route"
-      description "This action allows you to query route"
-  
-      param :id, "Id of route", :required => true, :type => Integer
-
-      error 'ValidationError', "The provided data was not sufficient to query route", :attributes => {:errors => "A hash of error details"}
-      error 'RouteIdMissing', "Route Id is missing"
-      error 'RouteNotFound', "The route not found"
-  
-      returns Hash, :structure => :route
-  
-      action do
-        route = identity.server.routes.find_by_id(params.id)
-        if route.nil?
-          error 'RouteNotFound'
-        else
+        route.mode = params.mode
+        route.created_at = Time.now
+        route.updated_at = Time.now
+        if route.save
           structure :route, route, :return => true
-        end
-      end
-    end
-
-    action :update do
-      title "Update route"
-      description "This action allows you to update route"
-
-      param :domain, "Domain name", :required => false, :type => String
-      param :endpoint_name, "Endpoint address (ex@mp.le)", :required => false, :type => String
-      param :endpoint_type, "Endpoint type: SMTPEndpoint | HTTPEndpoint | AddressEndpoint", :required => false, :type => String
-      param :spam_mode, "Route spam mode: Mark | Quarantine | Fail", :required => false, :type => String
-      param :mode, "Route mode: Endpoint | Accept | Hold | Bounce | Reject", :required => false, :type => String
-      param :name, "Name of name@example.com", :required => false, :type => String
-      param :id, "Id of route", :required => true, :type => Integer
-      
-      error 'ValidationError', "The provided data was not sufficient to query route", :attributes => {:errors => "A hash of error details"}
-      error 'RouteIdMissing', "Route id is missing"
-      # error 'InvalidRouteId', "Route name is invalid" Q1
-      error 'RouteNotFound', "The route not found"
-
-      returns Hash, :structure => :route
-
-      action do
-        route = identity.server.routes.find_by_id(params.id)
-        if route.nil?
-          error 'RouteNotFound'
         else
-          route.domain = params.domain if !params.domain.nil?
-          route.name = params.name if !params.name.nil?
-          route.spam_mode = params.spam_mode if !params.spam_mode.nil?
-          route.endpoint_id = identity.server.address_endpoints.find_by_address(params.endpoint_name).id if !params.endpoint_name.nil?
-          route.endpoint_type = params.endpoint_type if !params.endpoint_type.nil?
-          route.mode = params.mode if !params.mode.nil?
-          route.updated_at = Time.now
-          if route.save
-            structure :route, route, :return => true
+          error_message = route.errors.full_messages.first
+          if error_message == "Name is invalid"
+            error "InvalidRouteName"
           else
-            # error_message = route.errors.full_messages.first
-            # error "Unknown Error", error_message
-            error "Unknown Error", route.errors.full_messages.first
-            # TODO we search by id, so what validation shoud we put here for id?? Q1
-            # if error_message == "Name is invalid"
-            #   error "InvalidRouteName"
-            # else
-            #   error "Unknown Error", error_message
-            # end
+            error "Unknown Error", error_message
           end
         end
+      else
+        error 'RouteNameExists'
+      end
+
+    end
+  end
+  
+  action :query do
+    title "Query route"
+    description "This action allows you to query route"
+
+    param :domain, "Domain name", :required => true, :type => String
+    param :name, "Route name (max 50)", :required => true, :type => String
+
+    error 'ValidationError', "The provided data was not sufficient to query route", :attributes => {:errors => "A hash of error details"}
+    error 'RouteIdMissing', "Route Id is missing"
+    error 'RouteNotFound', "The route not found"
+
+    returns Hash, :structure => :route
+
+    action do
+      route = identity.server.routes.find_by_name_and_domain(params.name, params.domain)
+      if route.nil?
+        error 'RouteNotFound'
+      else
+        structure :route, route, :return => true
       end
     end
+  end
 
-    action :delete do
-      title "Delete a route"
-      description "This action allows you to delete route"
-  
-      param :id, "Id of route", :required => true, :type => Integer
+  action :update do
+    title "Update route"
+    description "This action allows you to update route"
 
-      error 'ValidationError', "The provided data was not sufficient to query route", :attributes => {:errors => "A hash of error details"}
-      error 'RouteIdMissing', "Route Id is missing"
-      error 'RouteNotFound', "The route not found"
-      error 'RouteNotDeleted', "Route could not be deleted"
-  
-      returns Hash
-  
-      action do
-        route = identity.server.routes.find_by_id(params.id)
-        if route.nil?
-          error 'RouteNotFound'
-        elsif route.delete
-          {:message => "Route deleted successfully"}
+    param :domain, "Domain name", :required => false, :type => String
+    param :endpoint_name, "Endpoint address (ex@mp.le)", :required => false, :type => String
+    param :endpoint_type, "Endpoint type: SMTPEndpoint | HTTPEndpoint | AddressEndpoint", :required => false, :type => String
+    param :spam_mode, "Route spam mode: Mark | Quarantine | Fail", :required => false, :type => String
+    param :mode, "Route mode: Endpoint | Accept | Hold | Bounce | Reject", :required => false, :type => String
+    param :name, "Name of name@example.com", :required => false, :type => String
+    param :id, "Id of route", :required => true, :type => Integer
+    
+    error 'ValidationError', "The provided data was not sufficient to query route", :attributes => {:errors => "A hash of error details"}
+    error 'RouteIdMissing', "Route id is missing"
+    error 'RouteNotFound', "The route not found"
+
+    returns Hash, :structure => :route
+
+    action do
+      route = identity.server.routes.find_by_id(params.id)
+      if route.nil?
+        error 'RouteNotFound'
+      else
+        route.domain = params.domain if !params.domain.nil?
+        route.name = params.name if !params.name.nil?
+        route.spam_mode = params.spam_mode if !params.spam_mode.nil?
+        route.endpoint_id = identity.server.address_endpoints.find_by_address(params.endpoint_name).id if !params.endpoint_name.nil?
+        route.endpoint_type = params.endpoint_type if !params.endpoint_type.nil?
+        route.mode = params.mode if !params.mode.nil?
+        route.updated_at = Time.now
+        if route.save
+          structure :route, route, :return => true
         else
-          error 'RouteNotDeleted'
+          error "Unknown Error", route.errors.full_messages.first
         end
       end
     end
   end
 
-  
+  action :delete do
+    title "Delete a route"
+    description "This action allows you to delete route"
+
+    param :domain, "Domain name", :required => true, :type => String
+    param :name, "Route name (max 50)", :required => true, :type => String
+
+    error 'ValidationError', "The provided data was not sufficient to query route", :attributes => {:errors => "A hash of error details"}
+    error 'RouteIdMissing', "Route Id is missing"
+    error 'RouteNotFound', "The route not found"
+    error 'RouteNotDeleted', "Route could not be deleted"
+
+    returns Hash
+
+    action do
+      route = identity.server.routes.find_by_name_and_domain(params.name, params.domain)
+      if route.nil?
+        error 'RouteNotFound'
+      elsif route.delete
+        {:message => "Route deleted successfully"}
+      else
+        error 'RouteNotDeleted'
+      end
+    end
+  end
+end
+
+
