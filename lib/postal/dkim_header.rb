71b059f9d2f149352fcc73e5c3cf5c9fb5344449
Dmitriy Alekseev <1865999+dragoangel@users.noreply.github.com>
2020-09-30 18:04:36 +0300

    Add common DKIM headers (#1218)
    Add commonly used headers to DKIM signatures

--- lib/postal/dkim_header.rb
+++ lib/postal/dkim_header.rb
@@ -32,7 +32,7 @@ module Postal
 
     def normalized_headers
       Array.new.tap do |new_headers|
-        headers.select { |h| h.match(/^(to|from|date|subject|message-id):/i) }.each do |h|
+        headers.select { |h| h.match(/^(from|sender|reply-to|subject|date|message-id|to|cc|mime-version|content-type|content-transfer-encoding|resent-to|resent-cc|resent-from|resent-sender|resent-message-id|in-reply-to|references|list-id|list-help|list-owner|list-unsubscribe|list-subscribe|list-post):/i) }.each do |h|
           new_headers << normalize_header(h)
         end
       end
