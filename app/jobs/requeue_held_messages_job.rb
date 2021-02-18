4061b73b9ab742f9a84687a4e4a543a8e1437451
Dmitriy Alekseev <1865999+dragoangel@users.noreply.github.com>
2021-02-18 17:54:15 +0200

    Update requeue_held_messages_job.rb

--- app/jobs/requeue_held_messages_job.rb
+++ app/jobs/requeue_held_messages_job.rb
@@ -1,7 +1,7 @@
 class RequeueHeldMessagesJob < Postal::Job
   def perform
     Server.all.each do |server|
-      if !server.queued_messages.first
+      if server.mode != "Development" && !server.queued_messages.first
         retry_limit = server.send_limit.nil? ? 200 : server.send_limit - server.throughput_stats[:outgoing]
         db = server.message_db
         query = { where: { held: 1 }, limit: retry_limit }
