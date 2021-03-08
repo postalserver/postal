719dd61a24a32ceb3a90b27f9e4d5093ea9c30f8
William Hall <william.hall@synergitech.co.uk>
2021-03-08 18:41:34 +0000

    update install scripts with new official rabbitmq location

--- script/install/ubuntu1604.sh
+++ script/install/ubuntu1604.sh
@@ -24,8 +24,8 @@ apt install -y software-properties-common
 apt-add-repository ppa:brightbox/ruby-ng -y
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
 add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.1/ubuntu xenial main'
-curl -sL https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add -
-add-apt-repository 'deb http://www.rabbitmq.com/debian/ testing main'
+wget -O - "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | sudo apt-key add -
+add-apt-repository 'deb https://packagecloud.io/rabbitmq/rabbitmq-server/ubuntu/ xenial main'
 apt update
 export DEBIAN_FRONTEND=noninteractive
 apt install -y ruby2.3 ruby2.3-dev build-essential libssl-dev mariadb-server libmysqlclient-dev rabbitmq-server nodejs git nginx wget nano
