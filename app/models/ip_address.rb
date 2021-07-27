0ba44dde405c7e923ae9f3e0e0fda89d8a717bab
Adam Cooke <me@adamcooke.io>
2021-07-27 21:25:07 +0000

    feat: add priorities to IP address assignment

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -9,6 +9,7 @@
 #  created_at :datetime
 #  updated_at :datetime
 #  hostname   :string(255)
+#  priority   :integer
 #
 
 class IPAddress < ApplicationRecord
@@ -18,5 +19,26 @@ class IPAddress < ApplicationRecord
   validates :ipv4, :presence => true, :uniqueness => true
   validates :hostname, :presence => true
   validates :ipv6, :uniqueness => {:allow_blank => true}
+  validates :priority, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, only_integer: true }
+
+  scope :order_by_priority, -> { order(priority: :desc) }
+
+  before_validation :set_default_priority
+
+  private
+
+  def set_default_priority
+    return if priority.present?
+
+    self.priority = 100
+  end
+
+  class << self
+
+    def select_by_priority
+      order(Arel.sql("RAND() * priority DESC")).first
+    end
+
+  end
 
 end
---
+++ db/migrate/20210727210551_add_priority_to_ip_addresses.rb
@@ -0,0 +1,6 @@
+class AddPriorityToIPAddresses < ActiveRecord::Migration[5.2]
+  def change
+    add_column :ip_addresses, :priority, :integer
+    IPAddress.where(priority: nil).update_all(priority: 100)
+  end
+end
