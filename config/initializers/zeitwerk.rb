3fb40e4e247893b314e42affa4604a7a71a52c59
Adam Cooke <adam@adam.ac>
2024-01-30 10:19:37 +0000

    fix: ignore message DB migrations in autoloader

---
+++ config/initializers/zeitwerk.rb
@@ -0,0 +1,5 @@
+Rails.autoloaders.each do |autoloader|
+  # Ignore the message DB migrations directory as it doesn't follow
+  # Zeitwerk's conventions and is always loaded and executed in order.
+  autoloader.ignore(Rails.root.join('lib/postal/message_db/migrations'))
+end
