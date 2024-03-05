2c20ba65f64ccb0f8174e3f523dedb3806478782
Adam Cooke <adam@krystal.io>
2024-03-05 22:11:35 +0000

    style(rubocop): use _ when not using a variable in helm config exporter

--- lib/postal/helm_config_exporter.rb
+++ lib/postal/helm_config_exporter.rb
@@ -12,7 +12,7 @@ module Postal
 
       @schema.groups.each do |group_name, group|
         path << group_name
-        group.attributes.each do |name, attr|
+        group.attributes.each do |name, _|
           env_var = Konfig::Sources::Environment.path_to_env_var(path + [name])
           contents << <<~VAR.strip
             {{ include "app.envVar" (dict "name" "#{env_var}" "spec" .Values.postal.#{path.join('.')}.#{name} "root" . ) }}
