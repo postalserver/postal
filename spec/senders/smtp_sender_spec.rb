e22d56601bff28632466a6ab91b0575a8857266c
jmdunsing <113212353+jmdunsing@users.noreply.github.com>
2024-03-06 15:58:35 -0600

    Update mail from (#17) (#18)
    * feat: Add message token to return path
    
    Adding the message token to the return path would be beneficial for users to link bounces to email addresses when X-Postal-MsgID was not included in the bounce message's headers.
    
    
    
    * test: update smtp_sender_spect.rb to reflect design changes to mail from
    
    
    
    ---------
    
    Signed-off-by: jmdunsing <113212353+jmdunsing@users.noreply.github.com>

--- spec/senders/smtp_sender_spec.rb
+++ spec/senders/smtp_sender_spec.rb
@@ -261,7 +261,7 @@ RSpec.describe SMTPSender do
             sender.send_message(message)
             expect(sender.endpoints.last).to have_received(:send_message).with(
               kind_of(String),
-              "#{server.token}@#{domain.return_path_domain}",
+              "#{server.token}+#{message.token}@#{domain.return_path_domain}",
               ["john@example.com"]
             )
           end
@@ -272,7 +272,7 @@ RSpec.describe SMTPSender do
             sender.send_message(message)
             expect(sender.endpoints.last).to have_received(:send_message).with(
               kind_of(String),
-              "#{server.token}@#{Postal::Config.dns.return_path_domain}",
+              "#{server.token}+#{message.token}@#{Postal::Config.dns.return_path_domain}",
               ["john@example.com"]
             )
           end
@@ -306,7 +306,7 @@ RSpec.describe SMTPSender do
           it "adds the resent-sender header" do
             sender.send_message(message)
             expect(sender.endpoints.last).to have_received(:send_message).with(
-              "Resent-Sender: #{server.token}@#{Postal::Config.dns.return_path_domain}\r\n#{message.raw_message}",
+              "Resent-Sender: #{server.token}+#{message.token}@#{Postal::Config.dns.return_path_domain}\r\n#{message.raw_message}",
               kind_of(String),
               kind_of(Array)
             )
