4e1deb2d2aeb61d9dddb3729916411c94e73c1c6
Adam Cooke <adam@krystal.io>
2024-03-06 14:05:44 +0000

    fix(web-server): allow for trusted proxies not be set

--- config/initializers/trusted_proxies.rb
+++ config/initializers/trusted_proxies.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rack::Request.ip_filter = lambda { |ip|
-  if Postal::Config.postal.trusted_proxies.any? { |net| net.include?(ip) } ||
+  if Postal::Config.postal.trusted_proxies&.any? { |net| net.include?(ip) } ||
      ip.match(/\A127\.0\.0\.1\Z|\A::1\Z|\Afd[0-9a-f]{2}:.+|\Alocalhost\Z|\Aunix\Z|\Aunix:/i)
     true
   else
