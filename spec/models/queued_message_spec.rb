b55becd2eca43a8485addb92c923595ffa3f74fd
Adam Cooke <adam@adam.ac>
2024-03-13 19:02:59 +0000

    doc: add annotations to queued message spec

--- spec/models/queued_message_spec.rb
+++ spec/models/queued_message_spec.rb
@@ -1,5 +1,30 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: queued_messages
+#
+#  id            :integer          not null, primary key
+#  attempts      :integer          default(0)
+#  batch_key     :string(255)
+#  domain        :string(255)
+#  locked_at     :datetime
+#  locked_by     :string(255)
+#  manual        :boolean          default(FALSE)
+#  retry_after   :datetime
+#  created_at    :datetime
+#  updated_at    :datetime
+#  ip_address_id :integer
+#  message_id    :integer
+#  route_id      :integer
+#  server_id     :integer
+#
+# Indexes
+#
+#  index_queued_messages_on_domain      (domain)
+#  index_queued_messages_on_message_id  (message_id)
+#  index_queued_messages_on_server_id   (server_id)
+#
 require "rails_helper"
 
 RSpec.describe QueuedMessage do
