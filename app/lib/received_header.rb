78d8d7198c40e19cbed75ee2f0d25eb3629ba8e3
wsdexe <67387055+wsdexe@users.noreply.github.com>
2025-01-25 02:25:03 +0500

    Update received_header.rb
    Signed-off-by: wsdexe <67387055+wsdexe@users.noreply.github.com>

--- app/lib/received_header.rb
+++ app/lib/received_header.rb
@@ -15,11 +15,11 @@ class ReceivedHeader
         raise Error, "`method` is invalid (must be one of #{OUR_HOSTNAMES.join(', ')})"
       end
 
-      header = "by #{helo} with #{method.to_s.upcase}; #{Time.now.utc.rfc2822}"
+      header = "by VS with HTTP; #{Time.now.utc.rfc2822}"
 
       if server.nil? || server.privacy_mode == false
         hostname = DNSResolver.local.ip_to_hostname(ip_address)
-        header = "from api (mail.#{helo} [#{ip_address}]) #{header}"
+        header = "from api (10-42-11-130.email.wikium-ru.svc.cluster.local [10.42.11.130])"
       end
 
       header
