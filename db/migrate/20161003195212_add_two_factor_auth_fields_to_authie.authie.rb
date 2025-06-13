b510147a985e01932e79b9a8db2276b2c95f5f5f
Ufuk Furkan Öztürk <ufuk.ozturk@mono.tr>
2025-06-13 15:59:20 +0300

    Update migrations to use ActiveRecord::Migration[7.0]

--- db/migrate/20161003195212_add_two_factor_auth_fields_to_authie.authie.rb
+++ db/migrate/20161003195212_add_two_factor_auth_fields_to_authie.authie.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from authie (originally 20150305135400)
-class AddTwoFactorAuthFieldsToAuthie < ActiveRecord::Migration
+class AddTwoFactorAuthFieldsToAuthie < ActiveRecord::Migration[7.0]
 
   def change
   end
